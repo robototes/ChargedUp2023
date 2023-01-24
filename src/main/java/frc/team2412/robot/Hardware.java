d68b2a81609a2205ab706eafbcae6e830d55122c
eddiert1239 <eddiert1239@gmail.com>
2023-01-23 19:05:34 -0800

    replacing miki's pull request for intake
    made intake subsystem and added motors and sensors and led strip to hardware

--- src/main/java/frc/team2412/robot/Hardware.java
+++ src/main/java/frc/team2412/robot/Hardware.java
@@ -3,37 +3,33 @@ package frc.team2412.robot;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Hardware {
-	public static final int PDP_ID = 1;
+    public static final int PDP_ID = 1;
 
-	// Drive devices are from range 1-19
-	public static final int DRIVEBASE_FRONT_LEFT_DRIVE_MOTOR = 1,
-			DRIVEBASE_FRONT_RIGHT_DRIVE_MOTOR = 4,
-			DRIVEBASE_BACK_LEFT_DRIVE_MOTOR = 7,
-			DRIVEBASE_BACK_RIGHT_DRIVE_MOTOR = 10;
-	public static final int DRIVEBASE_FRONT_LEFT_ANGLE_MOTOR = 2,
-			DRIVEBASE_FRONT_RIGHT_ANGLE_MOTOR = 5,
-			DRIVEBASE_BACK_LEFT_ANGLE_MOTOR = 8,
-			DRIVEBASE_BACK_RIGHT_ANGLE_MOTOR = 11;
-	public static final int DRIVEBASE_FRONT_LEFT_ENCODER_PORT = 3,
-			DRIVEBASE_FRONT_RIGHT_ENCODER_PORT = 6,
-			DRIVEBASE_BACK_LEFT_ENCODER_PORT = 9,
-			DRIVEBASE_BACK_RIGHT_ENCODER_PORT = 12;
-	public static final Rotation2d DRIVEBASE_FRONT_LEFT_ENCODER_OFFSET =
-			Rotation2d.fromDegrees(337.236);
-	public static final Rotation2d DRIVEBASE_FRONT_RIGHT_ENCODER_OFFSET =
-			Rotation2d.fromDegrees(251.982);
-	public static final Rotation2d DRIVEBASE_BACK_LEFT_ENCODER_OFFSET =
-			Rotation2d.fromDegrees(205.839);
-	public static final Rotation2d DRIVEBASE_BACK_RIGHT_ENCODER_OFFSET =
-			Rotation2d.fromDegrees(311.396);
-	public static final int GYRO_PORT = 13;
+    // Drive devices are from range 1-19
+    public static final int DRIVEBASE_FRONT_LEFT_DRIVE_MOTOR = 1, DRIVEBASE_FRONT_RIGHT_DRIVE_MOTOR = 4,
+            DRIVEBASE_BACK_LEFT_DRIVE_MOTOR = 7, DRIVEBASE_BACK_RIGHT_DRIVE_MOTOR = 10;
+    public static final int DRIVEBASE_FRONT_LEFT_ANGLE_MOTOR = 2, DRIVEBASE_FRONT_RIGHT_ANGLE_MOTOR = 5,
+            DRIVEBASE_BACK_LEFT_ANGLE_MOTOR = 8, DRIVEBASE_BACK_RIGHT_ANGLE_MOTOR = 11;
+    public static final int DRIVEBASE_FRONT_LEFT_ENCODER_PORT = 3, DRIVEBASE_FRONT_RIGHT_ENCODER_PORT = 6,
+            DRIVEBASE_BACK_LEFT_ENCODER_PORT = 9, DRIVEBASE_BACK_RIGHT_ENCODER_PORT = 12;
+    public static final Rotation2d DRIVEBASE_FRONT_LEFT_ENCODER_OFFSET = Rotation2d.fromDegrees(0); // TODO: find these
+    public static final Rotation2d DRIVEBASE_FRONT_RIGHT_ENCODER_OFFSET = Rotation2d.fromDegrees(0);
+    public static final Rotation2d DRIVEBASE_BACK_LEFT_ENCODER_OFFSET = Rotation2d.fromDegrees(0);
+    public static final Rotation2d DRIVEBASE_BACK_RIGHT_ENCODER_OFFSET = Rotation2d.fromDegrees(0);
+    public static final int GYRO_PORT = 13;
 
-	public static final double GEAR_RATIO;
+    // Intake devices are ports 30-39
+    public static final int INTAKE_MOTOR_1 = 30,
+            INTAKE_MOTOR_2 = 31;
 
-	static {
-		GEAR_RATIO =
-				Robot.getInstance().isCompetition()
-						? 0 // TBD
-						: 8.14; // L1 drive ratio
-	}
+    public static final int INTAKE_LED_STRIP = 6, INTAKE_DISTANCE_SENSOR = 7, INTAKE_COLOR_SENSOR = 8;
+
+    public static final double GEAR_RATIO;
+
+    static {
+        GEAR_RATIO = Robot.getInstance().isCompetition()
+                ? 0 // TBD
+                : 8.14; // L1 drive ratio
+    }
+
 }
--- src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/IntakeSubsystem.java
@@ -1,5 +1,108 @@
 package frc.team2412.robot.subsystems;
 
+import static frc.team2412.robot.Hardware.*;
+import static frc.team2412.robot.subsystems.IntakeSubsystem.IntakeConstants.*;
+import com.revrobotics.CANSparkMax.IdleMode;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IntakeSubsystem extends SubsystemBase {}
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+public class IntakeSubsystem extends SubsystemBase {
+    // CONSTANTS
+    public static class IntakeConstants {
+        // speeds
+        public static final double INTAKE_IN_SPEED = 0.5;
+        public static final double INTAKE_OUT_SPEED = 0.3;
+
+        public static final double INTAKE_CUBE_DISTANCE = 0;
+        public static final double INTAKE_CONE_DISTANCE = 0;
+        public static final double INTAKE_CUBE_COLOR = 0;
+        public static final double INTAKE_CONE_COLOR = 141;
+
+        // enums
+        public static enum GamePieceType {
+            CUBE, CONE, NONE;
+
+            /*
+             * for reference hi cammy eggy
+             * String example;
+             *
+             * GamePieceType(String example) {
+             * this.example = example;
+             * }
+             */
+
+        }
+
+        // public final cone;
+        // public final cube;
+        // public final nothing;
+
+    }
+
+    private final CANSparkMax motor1;
+    private final CANSparkMax motor2;
+    // private final ColorSensorV3 colorSensor;
+    // private final distanceSensorIDK distanceSensor;
+
+    // CONSTRUCTOR
+    public IntakeSubsystem() {
+        motor1 = new CANSparkMax(INTAKE_MOTOR_1, MotorType.kBrushless);
+        motor2 = new CANSparkMax(INTAKE_MOTOR_2, MotorType.kBrushless);
+        // colorSensor = new ColorSensorV3(INTAKE_COLOR_SENSOR); //to find I2C port
+        // distanceSensor = new ();
+
+        motor1.setIdleMode(IdleMode.kBrake);
+        motor2.setIdleMode(IdleMode.kBrake);
+    }   
+
+    // METHODS
+    public void setSpeed(double speed) {
+        setSpeed(speed, speed);
+    }
+
+    public void setSpeed(double firstSpeed, double secondSpeed) {
+        motor1.set(firstSpeed);
+        motor2.set(secondSpeed);
+    }
+
+    public double getSpeed() {
+        return motor1.get();
+    }
+
+    public void intakeIn() {
+        setSpeed(INTAKE_IN_SPEED);
+
+    }
+
+    public void intakeOut() {
+        setSpeed(INTAKE_OUT_SPEED);
+
+    }
+
+    public void intakeStop() {
+        setSpeed(0);
+
+    }
+
+    public GamePieceType detectType() {
+        // if () {
+        //     return GamePieceType.CUBE; 
+        // }
+        // else if () {
+        //     return GamePieceType.CONE; 
+        // }
+
+        return GamePieceType.NONE;
+    }
+
+    public boolean isSecured() {
+        return false;
+    } 
+
+    public void ledStrip(boolean buttonPressed) {
+    }
+
+}
