f1176047fb2c5a7380cfa3991d99b87a051d38e9
[object Object] <objekt-Objekt@proton.me>
2023-02-04 12:54:29 -0800

    first try part 1

--- src/main/java/frc/team2412/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/VisionSubsystem.java
@@ -4,48 +4,28 @@ import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.networktables.DoubleArraySubscriber;
-import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableEvent;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.team2412.robot.Hardware;
 import io.github.oblarg.oblog.annotations.Log;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.EnumSet;
+import java.util.Optional;
 import java.util.function.BiConsumer;
+
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 public class VisionSubsystem extends SubsystemBase {
 	private BiConsumer<Pose2d, Double> poseConsumer;
 	/** Null if no known robot pose, otherwise the last calculated robot pose from vision data. */
 	private Pose3d robotPose = null;
-	/*
-	 * Because we have to handle an IOException, we can't initialize fieldLayout in the variable declaration (private static final AprilTagFieldLayout fieldLayout = ...;). Instead, we have to initialize it in a static initializer (static { ... }).
-	 */
-	private static final AprilTagFieldLayout fieldLayout;
-
 	private DoubleArraySubscriber targetPose;
-
-	private static final NetworkTableEntry targetPoseEntry =
-			NetworkTableInstance.getDefault().getTable("limelight").getEntry("targetpose_robotspace");
-
-	static {
-		/*
-		 * This code runs when the class is initialized (same time as normal variable initializers).
-		 *
-		 * We have to use temp so that the compiler knows we initialize fieldLayout exactly once. If we directly initialize it, the compiler thinks we might initialize it twice if we get an IOException after initializing it in the try block (since then we'd run the catch block and initialize there too).
-		 */
-		AprilTagFieldLayout temp;
-
-		try {
-			temp = AprilTagFieldLayout.loadFromResource(AprilTagFields.kDefaultField.m_resourceFile);
-		} catch (IOException err) {
-			DriverStation.reportError("Could not load AprilTagFieldLayout! " + err, err.getStackTrace());
-			temp = null;
-		}
-		fieldLayout = temp;
-	}
 
 	public VisionSubsystem(BiConsumer<Pose2d, Double> poseConsumer) {
 		this.poseConsumer = poseConsumer;
@@ -58,58 +38,28 @@ public class VisionSubsystem extends SubsystemBase {
 		// 	networkTables.startClient4("localhost");
 		// }
 
-		this.targetPose = networkTables.getTable("limelight").getDoubleArrayTopic("targetpose_robotspace").subscribe(new double[] {});
-		//networkTables.addListener(
-		//		targetPoseEntry, EnumSet.of(NetworkTableEvent.Kind.kValueAll), this::updateEvent);
+		this.targetPose =
+				networkTables
+						.getTable("limelight")
+						.getDoubleArrayTopic("botpose_wpired") // TODO Get color from driverstation
+						.subscribe(new double[] {});
+		networkTables.addListener(
+				targetPose, EnumSet.of(NetworkTableEvent.Kind.kValueAll), this::processEvent);
 	}
 
-	//public void updateEvent(NetworkTableEvent event) {
-	@Override
-	public void periodic() {
+	public void processEvent(NetworkTableEvent event) {
+		// TODO Account for latency
+		var time = Timer.getFPGATimestamp();
 		double[] pose = targetPose.get();
-		System.out.print("Pose: ");
-		System.out.print(Arrays.toString(pose));
-		System.out.println();
+		robotPose = new Pose3d(pose[0], pose[1], pose[2], new Rotation3d(pose[3], pose[4], pose[5]));
+		// TODO 3D odometry
+		poseConsumer.accept(robotPose.toPose2d(), time);
 	}
 
 	@Log
 	public boolean hasTargets() {
 		return true;
 	}
-
-	/**
-	 * Calculates the robot pose relative to the field, using the given target.
-	 *
-	 * <p>Returns null if target is null or if no valid field pose could be found for the target
-	 * (either the target doesn't have an ID or we don't have a pose associated with the ID).
-	 *
-	 * @param target The target to use to calculate robot pose.
-	 * @return The calculated robot pose.
-	 */
-	/*public static Pose3d getRobotPoseUsingTarget(PhotonTrackedTarget target) {
-		if (target == null || fieldLayout == null) {
-			return null;
-		}
-		// If target doesn't have fiducial ID, value is -1 (which shouldn't be in the layout)
-		Optional<Pose3d> tagPose = fieldLayout.getTagPose(target.getFiducialId());
-		if (tagPose.isEmpty()) {
-			return null;
-		}
-		// getBestCameraToTarget() shouldn't be null
-		return tagPose
-				.get()
-				.transformBy(target.getBestCameraToTarget().inverse())
-				.transformBy(Hardware.CAM_TO_ROBOT);
-	}*/
-
-	/** Updates the robot pose cache. */
-	/*private void updatePoseCache() {
-		if (!hasTargets()) {
-			robotPose = null;
-		} else {
-			robotPose = getRobotPoseUsingTarget(latestResult.getBestTarget());
-		}
-	}*/
 
 	/**
 	 * Calculates the robot pose using the best target. Returns null if there is no known robot pose.
