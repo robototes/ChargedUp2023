70f7d930a9384bae072104186891018d75886e39
DriverStation1 <team@robototes.com>
2023-02-20 10:19:36 -0800

    start implementing auto chooser (probably used bad practice in the changed code but rohan wanted to pull the branch so i am pushing this) sorry team

--- src/main/java/frc/team2412/robot/commands/autonomous/PathPlannerTestCommand.java
+++ src/main/java/frc/team2412/robot/commands/autonomous/PathPlannerTestCommand.java
@@ -5,6 +5,8 @@ import com.pathplanner.lib.PathPlanner;
 import com.pathplanner.lib.PathPlannerTrajectory;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.team2412.robot.Robot;
+
+import java.util.HashMap;
 import java.util.List;
 
 public class PathPlannerTestCommand {
@@ -14,9 +16,10 @@ public class PathPlannerTestCommand {
 	 * @return the pathplanner auto command
 	 */
 	public static Command getAutoCommand() {
-		List<PathPlannerTrajectory> pathGroup =
-				PathPlanner.loadPathGroup("FullAuto", new PathConstraints(1, 0.3));
-		Command fullAuto = Robot.autoBuilder.fullAuto(pathGroup);
+		List<PathPlannerTrajectory> pathGroup =	PathPlanner.loadPathGroup("FullAuto", new PathConstraints(1, 0.3));
+		HashMap<String, Command> eventMap = new HashMap<String, Command>();
+		eventMap.put("AutoBalance", new AutoBalanceCommand(Robot.getInstance().subsystems.drivebaseSubsystem));
+		Command fullAuto = Robot.getInstance().getAutoBuilder(eventMap).fullAuto(pathGroup);
 		return fullAuto;
 	}
 }
