534ee70ecc22a2eb348c4235d60653d7bc29dde2
DriverStation2 <None>
2023-02-28 18:29:31 -0800

    fixed driver assist

---
+++ src/main/java/frc/team2412/robot/util/AutoBuilder.java
@@ -0,0 +1,9 @@
+package frc.team2412.robot.util;
+
+public class AutoBuilder {
+    enum StartingPosition {
+        
+    }
+
+    public static Command buildAuto()
+}
--- src/main/java/frc/team2412/robot/util/DriverAssist.java
+++ src/main/java/frc/team2412/robot/util/DriverAssist.java
@@ -15,34 +15,35 @@ import java.util.List;
 
 public class DriverAssist {
 	private static final PIDController ASSIST_TRANSLATION_PID = new PIDController(5.0, 0, 0);
-	private static final PIDController ASSIST_ROTATION_PID = new PIDController(1.0, 0, 0);
+	private static final PIDController ASSIST_ROTATION_PID = new PIDController(2.0, 0, 0);
 	private static final PathConstraints ASSIST_CONSTRAINTS = new PathConstraints(4.0, 2.0);
 	private static final double MAX_ALIGNMENT_DISTANCE_METERS = 100.0;
 
 	// comments are relative to the driver, with numbers increasing from right to left
 	private static final Pose2d[] alignmentPoses = {
-		new Pose2d(new Translation2d(2.0, 0.5), Rotation2d.fromDegrees(180)), // 1st cone scoring area
-		new Pose2d(new Translation2d(2.0, 1.05), Rotation2d.fromDegrees(180)), // 1st cube scoring area
-		new Pose2d(new Translation2d(2.0, 1.6), Rotation2d.fromDegrees(180)), // 2nd cone scoring area
-		new Pose2d(new Translation2d(2.0, 2.18), Rotation2d.fromDegrees(180)), // 3rd cone scoring area
-		new Pose2d(new Translation2d(2.0, 2.75), Rotation2d.fromDegrees(180)), // 2nd cube scoring area
-		new Pose2d(new Translation2d(2.0, 3.3), Rotation2d.fromDegrees(180)), // 4th cone scoring area
-		new Pose2d(new Translation2d(2.0, 3.86), Rotation2d.fromDegrees(180)), // 5th cone scoring area
-		new Pose2d(new Translation2d(2.0, 4.43), Rotation2d.fromDegrees(180)), // 3rd cube scoring area
-		new Pose2d(new Translation2d(2.0, 4.98), Rotation2d.fromDegrees(180)), // 6th cone scoring area
-		new Pose2d(new Translation2d(14.97965, 5.7531), Rotation2d.fromDegrees(0)), // Near slider
-		new Pose2d(new Translation2d(14.97965, 7.8359), Rotation2d.fromDegrees(0)), // Far slider
+		new Pose2d(new Translation2d(1.85, 0.5), Rotation2d.fromDegrees(180)), // 1st cone scoring area
+		new Pose2d(new Translation2d(1.85, 1.05), Rotation2d.fromDegrees(180)), // 1st cube scoring area
+		new Pose2d(new Translation2d(1.85, 1.6), Rotation2d.fromDegrees(180)), // 2nd cone scoring area
+		new Pose2d(new Translation2d(1.85, 2.18), Rotation2d.fromDegrees(180)), // 3rd cone scoring area
+		new Pose2d(new Translation2d(1.85, 2.75), Rotation2d.fromDegrees(180)), // 2nd cube scoring area
+		new Pose2d(new Translation2d(1.85, 3.3), Rotation2d.fromDegrees(180)), // 4th cone scoring area
+		new Pose2d(new Translation2d(1.85, 3.86), Rotation2d.fromDegrees(180)), // 5th cone scoring area
+		new Pose2d(new Translation2d(1.85, 4.43), Rotation2d.fromDegrees(180)), // 3rd cube scoring area
+		new Pose2d(new Translation2d(1.85, 4.98), Rotation2d.fromDegrees(180)), // 6th cone scoring area
 	};
-	private static final List<Pose2d> alignmentPosesList = List.of(alignmentPoses);
 
 	private static boolean isPoseTooFar(Pose2d robotPose, Pose2d alignmentPose) {
-		return robotPose.getTranslation().getDistance(alignmentPose.getTranslation())
-				> MAX_ALIGNMENT_DISTANCE_METERS;
+		if (robotPose.getTranslation().getDistance(alignmentPose.getTranslation())
+				> MAX_ALIGNMENT_DISTANCE_METERS) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	public static boolean alignRobot(DrivebaseSubsystem drivebaseSubsystem) {
 		Pose2d currentPose = drivebaseSubsystem.getPose();
-		Pose2d alignmentPose = currentPose.nearest(alignmentPosesList);
+		Pose2d alignmentPose = currentPose.nearest(List.of(alignmentPoses));
 		if (isPoseTooFar(currentPose, alignmentPose)) {
 			return false;
 		}
@@ -52,13 +53,16 @@ public class DriverAssist {
 						ASSIST_CONSTRAINTS,
 						new PathPoint(
 								currentPose.getTranslation(),
-								alignmentPose.minus(currentPose).getTranslation().getAngle(),
+								Rotation2d.fromRadians(
+										Math.atan2(
+												alignmentPose.getY() - currentPose.getY(),
+												alignmentPose.getX() - currentPose.getX())),
 								currentPose.getRotation(),
 								drivebaseSubsystem.getVelocity()),
 						new PathPoint(
 								alignmentPose.getTranslation(),
-								alignmentPose.getRotation(), // Direction of movement
-								alignmentPose.getRotation())); // Final heading
+								Rotation2d.fromDegrees(180),
+								alignmentPose.getRotation()));
 
 		Command followAlignmentCommand =
 				new PPSwerveControllerCommand(
