6b59c3d4552e6714400fc873931f4d1175ffca5f
Lucy <65467897+stewluj@users.noreply.github.com>
2023-03-11 13:23:57 -0800

    new paths and comp check in auto

--- src/main/java/frc/team2412/robot/util/auto/AutonomousTrajectories.java
+++ src/main/java/frc/team2412/robot/util/auto/AutonomousTrajectories.java
@@ -22,23 +22,27 @@ public class AutonomousTrajectories {
 		List<PathPlannerTrajectory> pathGroup =
 				PathPlanner.loadPathGroup(name, new PathConstraints(2, 2.0));
 		HashMap<String, Command> eventMap = new HashMap<String, Command>();
-		SetWristCommand wristOut =
-				new SetWristCommand(s.armSubsystem, s.intakeSubsystem, WristPosition.WRIST_SCORE);
-		SetWristCommand wristPrescore =
-				new SetWristCommand(s.armSubsystem, s.intakeSubsystem, WristPosition.WRIST_PRESCORE);
-		IntakeSetOutCommand intakeOut = new IntakeSetOutCommand(s.intakeSubsystem);
-		IntakeSetInCommand intakeIn = new IntakeSetInCommand(s.intakeSubsystem);
-		SetWristCommand wristIn =
-				new SetWristCommand(s.armSubsystem, s.intakeSubsystem, WristPosition.WRIST_RETRACT);
-		SequentialCommandGroup score =
-				new SequentialCommandGroup(intakeIn, wristOut, intakeOut.withTimeout(1.5), wristIn);
-		Command intake = new IntakeSetInCommand(s.intakeSubsystem).until(s.intakeSubsystem::isSecured);
-		eventMap.put("ScoreBottom", score);
 		eventMap.put(
 				"AutoBalance", new AutoBalanceCommand(Robot.getInstance().subsystems.drivebaseSubsystem));
-		eventMap.put("WristRetract", wristIn);
-		eventMap.put("WristPrescore", wristPrescore);
-		eventMap.put("IntakeOut", intakeOut);
+		if (!(s.intakeSubsystem == null) && !(s.armSubsystem == null)) {
+			SetWristCommand wristOut =
+					new SetWristCommand(s.armSubsystem, s.intakeSubsystem, WristPosition.WRIST_SCORE);
+			SetWristCommand wristPrescore =
+					new SetWristCommand(s.armSubsystem, s.intakeSubsystem, WristPosition.WRIST_PRESCORE);
+			IntakeSetOutCommand intakeOut = new IntakeSetOutCommand(s.intakeSubsystem);
+			IntakeSetInCommand intakeIn = new IntakeSetInCommand(s.intakeSubsystem);
+			SetWristCommand wristIn =
+					new SetWristCommand(s.armSubsystem, s.intakeSubsystem, WristPosition.WRIST_RETRACT);
+			SequentialCommandGroup score =
+					new SequentialCommandGroup(intakeIn, wristOut, intakeOut.withTimeout(1.5), wristIn);
+			Command intake =
+					new IntakeSetInCommand(s.intakeSubsystem).until(s.intakeSubsystem::isSecured);
+			eventMap.put("ScoreBottom", score);
+
+			eventMap.put("WristRetract", wristIn);
+			eventMap.put("WristPrescore", wristPrescore);
+			eventMap.put("IntakeOut", intakeOut);
+		}
 		Command fullAuto = Robot.getInstance().getAutoBuilder(eventMap).fullAuto(pathGroup);
 		return fullAuto;
 	}
