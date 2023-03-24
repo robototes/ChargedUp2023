6a6890bd877a9c46c7df7b5815663c9a13e03902
Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
2023-03-23 17:29:15 -0700

    Exclude measurements when too far from tag

--- src/main/java/frc/team2412/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/VisionSubsystem.java
@@ -24,6 +24,7 @@ import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonPoseEstimator;
 import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.targeting.PhotonPipelineResult;
 
 /**
  * All poses and transforms use the NWU (North-West-Up) coordinate system, where +X is
@@ -52,11 +53,13 @@ public class VisionSubsystem extends SubsystemBase {
 							new Translation3d(0, 0, Units.inchesToMeters(28)),
 							// Camera's upside down
 							new Rotation3d(Math.toRadians(180), 0, 0));
+	public static final double MAX_TRUSTABLE_HORIZONTAL_DISTANCE = 1;
 
 	private PhotonCamera photonCamera;
 	private Optional<EstimatedRobotPose> latestPose = Optional.empty();
 	private PhotonPoseEstimator photonPoseEstimator;
 	private final SwerveDrivePoseEstimator poseEstimator;
+	private boolean targetTooFar = false;
 
 	private double lastTimestampSeconds = 0;
 	/*
@@ -115,17 +118,30 @@ public class VisionSubsystem extends SubsystemBase {
 				.withSize(8, 1);
 		visionTab
 				.addDouble("Last timestamp", this::getLastTimestampSeconds)
+				.withPosition(1, 1)
+				.withSize(1, 1);
+		visionTab
+				.addBoolean("Target was too far", () -> targetTooFar)
 				.withPosition(0, 1)
 				.withSize(1, 1);
 	}
 
 	public void updateEvent(NetworkTableEvent event) {
-		latestPose = photonPoseEstimator.update();
+		PhotonPipelineResult pipelineResult = photonCamera.getLatestResult();
+		targetTooFar =
+				pipelineResult.getTargets().stream()
+						.noneMatch(
+								(target) ->
+										target.getBestCameraToTarget().getTranslation().toTranslation2d().getNorm()
+												< MAX_TRUSTABLE_HORIZONTAL_DISTANCE);
+		latestPose = photonPoseEstimator.update(pipelineResult);
 		if (latestPose.isPresent()) {
 			lastTimestampSeconds = latestPose.get().timestampSeconds;
-			synchronized (poseEstimator) {
-				poseEstimator.addVisionMeasurement(
-						latestPose.get().estimatedPose.toPose2d(), lastTimestampSeconds);
+			if (!targetTooFar) {
+				synchronized (poseEstimator) {
+					poseEstimator.addVisionMeasurement(
+							latestPose.get().estimatedPose.toPose2d(), lastTimestampSeconds);
+				}
 			}
 		}
 	}
