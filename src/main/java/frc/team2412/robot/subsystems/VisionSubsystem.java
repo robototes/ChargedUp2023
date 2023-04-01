d47bc7aa8600fcb0e66b4eb192c3f33e99f4c271
DriverStation3 <programming@robototes.com>
2023-03-31 20:35:24 -0700

    Update bonk LL position again

--- src/main/java/frc/team2412/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/team2412/robot/subsystems/VisionSubsystem.java
@@ -57,14 +57,14 @@ public class VisionSubsystem extends SubsystemBase {
 							new Rotation3d(0, 0, Math.toRadians(-6.7)))
 					: new Transform3d(
 							new Translation3d(
-									// 15 5/8 inches from back of robot, back is -half of length (24 in.)
-									Units.inchesToMeters(-24.0 / 2 + (15 + 5.0 / 8)),
+									// 4 3/4 inches from front of robot, front is +half of length (24 in.)
+									Units.inchesToMeters(24.0 / 2 - 4.75),
 									// 7 1/8 inches from left of robot, left is +half of width (24 in.)
 									Units.inchesToMeters(24.0 / 2 - (7 + 1.0 / 8)),
 									// 41.5 inches above the ground
 									Units.inchesToMeters(41.5)),
 							// Limelight's facing backwards
-							new Rotation3d(0, 0, Math.toRadians(180)));
+							new Rotation3d(0, 0, 0));
 	public static final double MAX_TRUSTABLE_HORIZONTAL_DISTANCE = 3;
 	// This is from the metric approximations from section 5.1 of the game manual
 	private static final double FIELD_LENGTH_METERS = 16.54;
