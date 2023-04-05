c19b21594c79ba77a92098995d106215936b892b
Jonah <58869582+jbko6@users.noreply.github.com>
2023-04-04 20:59:26 -0700

    Vision+alignment (#79)
    * Add double substation, spotless
    
    * Slight code cleanup
    
    * Spotless
    
    * Dynamic heading
    
    * fixed driver assist
    
    * remove unused path
    
    * resolve control button conflict
    
    * remove oopsie
    
    * spotless
    
    * Convert to field coordinates
    
    * Fix code errors
    
    * Add pose ShuffleboardLayout factory
    
    * Switch to multi-tap PNP
    
    * Update bonk position
    
    * Update bonk LL position again
    
    * Update WPILib and vendordeps
    
    * Remove Oblog (had different opencv version)
    
    * Report Pose3d degrees instead of radians
    
    * Spotless
    
    * Update limelight position
    
    * Clean up Bonk LL position constant
    
    * Change updateEvent to update
    
    * Shuffleboard organization
    
    * Framework for dynamic std devs
    
    * Merge from main
    
    * Move distance check to getStdDevs, decrease max distance
    
    * Add ambiguity filter
    
    * Add filter for when corner too close to edge
    
    * Fix threshold logic errors, change max distance
    
    * add some forgotten stuff merging
    
    * Don't repeatedly grab alliance
    
    * Properly flip based on alliance
    
    * Put vision field pose (without odometry) to Field2d
    
    * Set alliance in teleopInit, invert rotation when red
    
    * Why does this operator exist
    
    * seperate cube and cone
    
    * Why does this operator exist
    
    * Make Use vision measurements persistent
    
    * Enable use vision measurements in teleop
    
    * pose adjustments and more vision pulls
    
    ---------
    
    Co-authored-by: Joseph Eng <91924258+KangarooKoala@users.noreply.github.com>
    Co-authored-by: DriverStation2 <None>
    Co-authored-by: DriverStation3 <programming@robototes.com>
    Co-authored-by: [object Object] <objekt-Objekt@proton.me>

---
+++ src/main/java/frc/team2412/robot/util/DriverAssist.java
@@ -0,0 +1,78 @@
+package frc.team2412.robot.util;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPoint;
+import com.pathplanner.lib.commands.PPSwerveControllerCommand;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.team2412.robot.subsystems.DrivebaseSubsystem;
+import frc.team2412.robot.subsystems.IntakeSubsystem.IntakeConstants.GamePieceType;
+import java.util.List;
+
+public class DriverAssist {
+	private static final PIDController ASSIST_TRANSLATION_PID = new PIDController(10.0, 0, 0);
+	private static final PIDController ASSIST_ROTATION_PID = new PIDController(8.0, 0, 0);
+	private static final PathConstraints ASSIST_CONSTRAINTS = new PathConstraints(4.0, 2.0);
+
+	// comments are relative to the driver, with numbers increasing from right to left
+	private static final Pose2d[] CUBE_ALIGNMENT_POSES = {
+		new Pose2d(new Translation2d(1.90, 1.05), Rotation2d.fromDegrees(180)), // 1st cube scoring area
+		new Pose2d(new Translation2d(1.90, 2.75), Rotation2d.fromDegrees(180)), // 2nd cube scoring area
+		new Pose2d(new Translation2d(1.90, 4.43), Rotation2d.fromDegrees(180)), // 3rd cube scoring area
+	};
+
+	private static final Pose2d[] CONE_ALIGNMENT_POSES = {
+		new Pose2d(new Translation2d(1.80, 0.5), Rotation2d.fromDegrees(180)), // 1st cone scoring area
+		new Pose2d(new Translation2d(1.80, 1.6), Rotation2d.fromDegrees(180)), // 2nd cone scoring area
+		new Pose2d(new Translation2d(1.80, 2.18), Rotation2d.fromDegrees(180)), // 3rd cone scoring area
+		new Pose2d(new Translation2d(1.80, 3.3), Rotation2d.fromDegrees(180)), // 4th cone scoring area
+		new Pose2d(new Translation2d(1.80, 3.86), Rotation2d.fromDegrees(180)), // 5th cone scoring area
+		new Pose2d(new Translation2d(1.80, 5), Rotation2d.fromDegrees(180)), // 6th cone scoring area
+	};
+
+	public static Command alignRobot(
+			DrivebaseSubsystem drivebaseSubsystem, GamePieceType gamePieceType) {
+		Pose2d currentPose = drivebaseSubsystem.getPose();
+		Pose2d alignmentPose =
+				currentPose.nearest(
+						List.of(
+								(gamePieceType == GamePieceType.CUBE)
+										? CUBE_ALIGNMENT_POSES
+										: CONE_ALIGNMENT_POSES));
+
+		PathPlannerTrajectory alignmentTraj =
+				PathPlanner.generatePath(
+						ASSIST_CONSTRAINTS,
+						new PathPoint(
+								currentPose.getTranslation(),
+								Rotation2d.fromRadians(
+										Math.atan2(
+												alignmentPose.getY() - currentPose.getY(),
+												alignmentPose.getX() - currentPose.getX())),
+								currentPose.getRotation(),
+								drivebaseSubsystem.getVelocity()),
+						new PathPoint(
+								alignmentPose.getTranslation(),
+								Rotation2d.fromDegrees(180),
+								alignmentPose.getRotation()));
+
+		Command followAlignmentCommand =
+				new PPSwerveControllerCommand(
+						alignmentTraj,
+						drivebaseSubsystem::getPose,
+						DrivebaseSubsystem.kinematics,
+						ASSIST_TRANSLATION_PID,
+						ASSIST_TRANSLATION_PID,
+						ASSIST_ROTATION_PID,
+						drivebaseSubsystem::drive,
+						true,
+						drivebaseSubsystem);
+
+		return followAlignmentCommand;
+	}
+}
